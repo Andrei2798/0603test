426017eb885afd3c4eb19b6557e94fd7121a5478
Andrei Nekrash <andrei.nekrasch@yandex.ru>
2024-03-07 00:26:11 +0300

    vite.config

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+export default defineConfig({
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        // enter: resolve(__dirname, "static/enter.html"),
+        main: resolve(__dirname, "static/registration.html"),
+      },
+    },
+  },
+});
