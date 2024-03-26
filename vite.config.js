93f6c2b321048e38d0683e7f013d55873331e927
Andrei Nekrash <andrei.nekrasch@yandex.ru>
2024-03-26 15:25:54 +0300

    hello

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     outDir: "dist",
     rollupOptions: {
       input: {
-        users: resolve("src/users.js"),
+        // users: resolve("src/users.js"),
         item: resolve("src/item.js"),
         index: resolve("src/index.js"),
         enter: resolve("src/enter.js"),
