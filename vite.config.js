b92a496d239dbf3f147902806ddc4ec00611bea5
Andrei Nekrash <andrei.nekrasch@yandex.ru>
2024-03-26 15:36:15 +0300

    ffd

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,36 @@
+// import { defineConfig } from "vite";
+// import { resolve } from "path";
+
+// export default defineConfig({
+//   build: {
+//     target: "esnext",
+//     outDir: "dist",
+//     rollupOptions: {
+//       input: {
+//         // users: resolve("src/users.js"),
+//         item: resolve("src/item.js"),
+//         index: resolve("src/index.js"),
+//         enter: resolve("src/enter.js"),
+//         registration: resolve("src/registration.js"),
+//       },
+//     },
+//     esbuildOptions: {
+//       target: "esnext", // или "es2020"
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import { resolve } from "path";
-
 export default defineConfig({
   build: {
-    target: "esnext",
     outDir: "dist",
     rollupOptions: {
       input: {
-        // users: resolve("src/users.js"),
-        item: resolve("src/item.js"),
-        index: resolve("src/index.js"),
-        enter: resolve("src/enter.js"),
-        registration: resolve("src/registration.js"),
+        index: resolve(__dirname, "index.html"),
+        enter: resolve(__dirname, "enter.html"),
+        registration: resolve(__dirname, "registration.html"),
       },
-    },
-    esbuildOptions: {
-      target: "esnext", // или "es2020"
     },
   },
 });
